9a6521efcf4bb698468e8720870394443f14c4d4
Oliver Atherton <118294630+olivrrcode@users.noreply.github.com>
2025-01-30 17:16:46 +0000

    feat: changed the layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+
 import Providers from './providers';
 
-const inter = Inter({ subsets: ['latin'] });
+const _inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Interview Preparation App',
