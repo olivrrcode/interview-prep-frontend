6f371b589f146bb7ef1a7cadb68cb865d98c6d95
Oliver Atherton <118294630+olivrrcode@users.noreply.github.com>
2025-01-30 12:26:17 +0000

    chore: working on frontend query functionality to work on backend

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-export default function Home() {
-  const queryClient = new QueryClient();
-
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
-}
